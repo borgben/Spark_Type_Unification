48a864239170fcc29f2f1d93a192854e94098995
borgben <benjaminborg9@gmail.com>
2023-05-15 13:05:23 +0200

    Unifying Nested Structs

--- merge_schema.scala
+++ merge_schema.scala
@@ -4,68 +4,62 @@ import org.apache.spark.sql.functions._
 
 class MyCustomException(s: String) extends Exception(s) {} 
 
-    def insert(list: List[StructField], i: Int, value: StructField): List[StructField] = list match {
+def insert(list: List[StructField], i: Int, value: StructField): List[StructField] = list match {
     case head :: tail if i > 0 => head :: insert(tail, i-1, value)
     case _ => value :: list
-    }
+}
 
-     def get_type_difference (type_pair:(DataType,DataType)):List[StructField] = 
-     {
-       type_pair match 
-       {
-         case (ArrayType(subtype_a,contains_null_a), ArrayType(subtype_b, contains_null_b)) => get_type_difference(subtype_a,subtype_b)
-         case (StructType(struct_fields_a),StructType(struct_fields_b)) => struct_fields_a.diff(struct_fields_b).toList
-         case _ => Nil
-       }
-     }
 
-     def unify_struct_fields(struct_field_a:Seq[StructField], struct_field_b:Seq[StructField])
+def unify_struct_fields(struct_field_a:Seq[StructField], struct_field_b:Seq[StructField]):Seq[StructField] = 
      {
        // Create Empty Sequence.
-       var unified_sequence = Seq()
+       var unified_sequence:Seq[StructField] = Seq()
 
        // Convert "struct_field_a" and "struct_field_b" to a map.
-       val map_a = struct_field_a.map((x:StructField) => x match{ case(StructField(name,_, _)) => (name,x)}).toMap 
-       val map_b = struct_field_b.map((x:StructField) => x match{ case(StructField(name,_, _)) => (name,x)}).toMap 
+       val map_a: Map[String,StructField] = struct_field_a.map((x:StructField) => x match{ case(StructField(name,_, _,_)) => (name,x)}).toMap 
+       val map_b: Map[String,StructField] = struct_field_b.map((x:StructField) => x match{ case(StructField(name,_, _,_)) => (name,x)}).toMap 
 
        // Retrieve the difference between the two maps.
-       val map_diff = (map_a.keySet -- map_b.keySet) ++ (map_a.keySet -- map_b.keySet)
+       val a_not_b: Set[String] = (map_a.keySet -- map_b.keySet)
+       val b_not_a: Set[String] = (map_b.keySet -- map_a.keySet)
+
+       val seq_a_not_b: Seq[StructField] = struct_field_a.filter((x:(StructField)) => x match{case StructField(name,_,_,_) => a_not_b contains name })
+       val seq_b_not_a: Seq[StructField] = struct_field_b.filter((x:(StructField)) => x match{case StructField(name,_,_,_) => b_not_a contains name })
+
+       val seq_diff: Seq[StructField] = seq_a_not_b ++ seq_b_not_a
 
        // Iterate the elements of "struct_field_b" looking for elements with matching names within "map_a". 
        // If there exist matching elements check whether their datatypes match if not they must be unified.
        // By the end of the for loop "unified_sequence" will contain all the matching elements between "struct_field_a" and "struct_field_b"
        for (x <- struct_field_b)
        {
-          struct_field_b match 
+          x match 
           {
-              case (StructField(name_b,data_type_b, contains_null_b)) => 
+              case (StructField(name_b,data_type_b, contains_null_b, _)) => 
               {
-                if (map_a contains name_b)
-                {
                     map_a.get(name_b) match 
                     {
-                        case (StructField(_,data_type_a,contains_null_a)) => 
+                        case (Some(StructField(_,data_type_a,contains_null_a, _))) => 
                         {
                           if (data_type_a == data_type_b)
                           {
-                            unified_sequence = unified_sequence.add(StructField(name_b,data_type_a,contains_null_a && contains_null_b))
+                            unified_sequence = unified_sequence :+ StructField(name_b,data_type_a,contains_null_a && contains_null_b)
                           }
                           else
                           {
-                            unified_sequence = unified_sequence.add(StructField(name_b , unify_spark_types((data_type_a,data_type_b)),contains_null_a && contains_null_b))
+                            unified_sequence = unified_sequence :+ StructField(name_b , unify_spark_types((data_type_a,data_type_b)),contains_null_a && contains_null_b)
                           }
                         }
+                        case None => unified_sequence=unified_sequence
                     }
-                }
+
               }
           }
        }
-       
-       // TODO: We need to convert map_diff to a sequence and replace "struct_field_a" with the new "struct_field_diff".
-       return struct_field_a.union(unified_sequence).distinct
+       return seq_diff.union(unified_sequence).distinct
      }
 
-     def unify_spark_types (type_pair:(DataType,DataType)):DataType = 
+def unify_spark_types (type_pair:(DataType,DataType)):DataType = 
      {
        type_pair match 
        {
@@ -75,12 +69,14 @@ class MyCustomException(s: String) extends Exception(s) {}
          case (BooleanType, BooleanType) => BooleanType
          case (TimestampType, TimestampType) => TimestampType
          case (ArrayType(subtype_a,contains_null_a), ArrayType(subtype_b, contains_null_b)) => ArrayType(unify_spark_types((subtype_a,subtype_b)), contains_null_a && contains_null_b)
-         case (StructType(struct_fields_a),StructType(struct_fields_b)) => StructType(struct_fields_a.union(struct_fields_b).distinct)
-         case _ => ShortType
+         case (StructType(struct_fields_a),StructType(struct_fields_b)) => StructType(unify_struct_fields(struct_fields_a, struct_fields_b))
+         case (ArrayType(subtype_a,contains_null_a), data_type_b) => ArrayType(unify_spark_types((subtype_a,data_type_b)),contains_null_a)
+         case (data_type_a, ArrayType(subtype_b,contains_null_b)) => ArrayType(unify_spark_types((subtype_b,data_type_a)),contains_null_b)
+         case (data_type_a, data_type_b) => throw new MyCustomException("Exception caught in unify_spark_df, " + (data_type_a).toString + " cannot match " + data_type_b.toString +".")
        }
      }
 
-    def merge_struct_fields(column_name:Char, struct_field_a:Seq[StructField], struct_field_b:Seq[StructField]):(String, String)= 
+def merge_struct_fields(column_name:Char, struct_field_a:Seq[StructField], struct_field_b:Seq[StructField]):(String, String)= 
     {
         var output_string = ""
         var output_string_ = ""
@@ -189,7 +185,7 @@ class MyCustomException(s: String) extends Exception(s) {}
         }
     }
 
-    def merge_complex_spark_types (column_name:String, anon_var:Int = 97,type_pair:(DataType,DataType)):(String,String) = 
+def merge_complex_spark_types (column_name:String, anon_var:Int = 97,type_pair:(DataType,DataType)):(String,String) = 
      {
        type_pair match 
        {
@@ -211,7 +207,7 @@ class MyCustomException(s: String) extends Exception(s) {}
        }
      }
 
-    def merge_spark_df(column_diff:List[(String, DataType, DataType)]):(String,String) = 
+def merge_spark_df(column_diff:List[(String, DataType, DataType)]):(String,String) = 
     {
         for (x <- column_diff)
         {
@@ -226,8 +222,8 @@ class MyCustomException(s: String) extends Exception(s) {}
         return ("","")
     }
 
-     // We assume the lists are the same size 
-     def unify_spark_df(zipped_type_list:List[((String, DataType),(String, DataType))]): List[(String, DataType, DataType)]=
+// We assume the lists are the same size 
+def unify_spark_df(zipped_type_list:List[((String, DataType),(String, DataType))]): List[(String, DataType, DataType)]=
      {
        zipped_type_list match 
        {
@@ -255,58 +251,58 @@ class MyCustomException(s: String) extends Exception(s) {}
        }
      }
 
-  val structureData = Seq(
-    Row(Seq(Row("James ","Vella","Smith")),"36636","M",3100),
-    Row(Seq(Row("Michael ","J","Rose","")),"40288","M",4300),
-    Row(Seq(Row("Robert ","Sebastian","Williams")),"42114","M",1400)
+val structureData = Seq(
+    Row(Seq(Row("James ",Seq(Row("Vella")),"Smith")),"36636","M",3100),
+    Row(Seq(Row("Michael ",Seq(Row("J")),"Rose")),"40288","M",4300),
+    Row(Seq(Row("Robert ",Seq(Row("Sebastian")),"Williams")),"42114","M",1400)
   )
 
-  val structureData2 = Seq(
-        Row(Seq(Row("Maria ",Seq(Row("Anne"),Row("Frank")),"Jones")),"39192","F",5500),
-        Row(Seq(Row("Jen",Seq(Row("Mary"),Row("Allen")),"Brown")),"","F",-1)
+val structureData2 = Seq(
+        Row(Seq(Row("Maria ",Seq(Row("Anne",""),Row("Frank","")),"Jones")),"39192","F",5500),
+        Row(Seq(Row("Jen",Seq(Row("Mary",""),Row("Allen","Burn")),"Brown")),"","F",-1)
   )
 
-  val structureSchema = new StructType()
-    .add("name",new ArrayType(new StructType().add("firstname",StringType).add("middlename", StringType).add("lastname",StringType),false))
+val structureSchema = new StructType()
+    .add("name",new ArrayType(new StructType().add("firstname",StringType).add("middlename", new ArrayType(new StructType().add("middlename0",StringType),false)).add("lastname",StringType),false))
     .add("id",StringType)
     .add("gender",StringType)
     .add("salary",IntegerType)
 
-  val structureSchema2 = new StructType()
-    .add("name",new ArrayType(new StructType().add("firstname",StringType).add("middlename",new ArrayType(new StructType().add("name1",StringType),false)).add("lastname",StringType),false))
+val structureSchema2 = new StructType()
+    .add("name",new ArrayType(new StructType().add("firstname",StringType).add("middlename",new ArrayType(new StructType().add("middlename0",StringType).add("middlename1",StringType),false)).add("lastname",StringType),false))
     .add("id",StringType)
     .add("gender",StringType)
     .add("salary",IntegerType)
 
-  val df = spark.createDataFrame(
+val df = spark.createDataFrame(
      spark.sparkContext.parallelize(structureData),structureSchema)
   df.printSchema()
   df.show()
 
-    val df2 = spark.createDataFrame(
+val df2 = spark.createDataFrame(
      spark.sparkContext.parallelize(structureData2),structureSchema2)
   df2.printSchema()
   df2.show()
 
-  val df_datatypes_cols = df.columns.map(str => col(str))
-  val df2_datatypes_cols = df2.columns.map(str => col(str))
+val df_datatypes_cols = df.columns.map(str => col(str))
+val df2_datatypes_cols = df2.columns.map(str => col(str))
 
-  val df_datatypes  = df.select(df_datatypes_cols:_*).schema.fields.map(f => (f.name,f.dataType)).toList
-  val df2_datatypes = df2.select(df2_datatypes_cols:_*).schema.fields.map(f => (f.name,f.dataType)).toList
+val df_datatypes  = df.select(df_datatypes_cols:_*).schema.fields.map(f => (f.name,f.dataType)).toList
+val df2_datatypes = df2.select(df2_datatypes_cols:_*).schema.fields.map(f => (f.name,f.dataType)).toList
 
-  val unified_schema = unify_spark_df(df_datatypes zip df2_datatypes)
+val unified_schema = unify_spark_df(df_datatypes zip df2_datatypes)
 
-  println(unified_schema.toString)
+println(unified_schema.toString)
 
 
 
-// merge_spark_df(unify_spark_df(df_datatypes zip df2_datatypes)) match 
-// {
-//     case (output_string,output_string_) => 
-//     {
-//       val df3 = df.withColumn("name",expr(output_string)).union(df2.withColumn("name",expr(output_string_)))
+merge_spark_df(unify_spark_df(df_datatypes zip df2_datatypes)) match 
+{
+    case (output_string,output_string_) => 
+    {
+      val df3 = df.withColumn("name",expr(output_string)).union(df2.withColumn("name",expr(output_string_)))
 
-//       df3.printSchema()
-//       df3.show()
-//     }
-// }
+      df3.printSchema()
+      df3.show()
+    }
+}
